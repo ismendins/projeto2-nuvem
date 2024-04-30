06120800669b52d6ddac78a084fb6fb30b09a462
Isabella Mendes <107645348+ismendins@users.noreply.github.com>
2024-04-30 17:51:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-
 app.get("/", function(req,res){
-    res.send("<h1>Sorveteria Diamante!</h1> <h2>Oferecemos os seguintes sabores: </h2> <br>- Chocolate <br>- Manga <br>- Morango <br>- Napolitano <br><h3>E muito mais. Venha nos visitar!</h3>");
+    res.send("<h1>Sorveteria Diamante!</h1> <h2>Oferecemos os seguintes sabores: </h2> <br>- Chocolate <br>- Manga <br>- Morango <br>- Napolitano <br><h3>Também temos doces e bolos. Venha nos visitar!</h3>");
 })
 
+
 app.get("/sabores", function(req,res){
-    res.send("<h2> Lista de sabores completa: </h2> <br>- Chocolate <br>- Manga <br>- Morango <br>- Napolitano <br>- Açaí <br>- Limão <br>- Cupuaçu <br>- Tapioca");
+    res.send("<h2> Lista de sabores completa: </h2> <br>- Chocolate <br>- Manga <br>- Morango <br>- Napolitano <br>- Açaí <br>- Limão <br>- Cupuaçu <br>- Tapioca <br> <h2>Doces: </h2><br>- Bem casados <br>- Brigadeiro <br>- Quindim");
 })
 
 app.get("/consulta/:parametro", function(req,res){
@@ -24,11 +24,10 @@ app.get("/cadastro/:sabor?", function(req,res){
     
 })
 
-app.listen(4000,function(erro){
+app.listen(3000,function(erro){
     if (erro){
         console.log("Erro ao Iniciar.");
     }else{
         console.log("Servidor Iniciado.");
     }
 })
-
