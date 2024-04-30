6eb36d3d9a4c14ed6848a6ff8caf98e36b7a39bd
Isabella Mendes <107645348+ismendins@users.noreply.github.com>
2024-04-30 17:40:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const express = require("express"); //importa o mÃ³dulo express neste arquivo
-const app = express(); //iniciando o express
+const express = require("express");
+const app = express();
 
-//criando a rota inicial
+
 app.get("/", function(req,res){
     res.send("<h1>Sorveteria Diamante!</h1> <h2>Oferecemos os seguintes sabores: </h2> <br>- Chocolate <br>- Manga <br>- Morango <br>- Napolitano <br><h3>E muito mais. Venha nos visitar!</h3>");
 })
 
-//rota do cadastro de produtos
 app.get("/sabores", function(req,res){
     res.send("<h2> Lista de sabores completa: </h2> <br>- Chocolate <br>- Manga <br>- Morango <br>- Napolitano <br>- Açaí <br>- Limão <br>- Cupuaçu <br>- Tapioca");
 })
 
-//rota com parametro 
 app.get("/consulta/:parametro", function(req,res){
-    //req --> dados enviados pelo cliente
-    //res --> resposta enviada pelo servidor de volta ao cliente
     res.send("Verifique na lista se o sabor " + req.params.parametro + " está cadastrado.");
 })
 
-//rota com parametro opcional
 app.get("/cadastro/:sabor?", function(req,res){
-    //req --> dados enviados pelo cliente
     var sabor = req.params.sabor;
     if (sabor){
         res.send("<h3>Sabor " + sabor + " cadastrado no sistema!</h3>");
@@ -30,7 +24,7 @@ app.get("/cadastro/:sabor?", function(req,res){
     
 })
 
-app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicaÃ§Ã£o na porta 3000
+app.listen(process.env.PORT ?? 3000,function(erro){
     if (erro){
         console.log("Erro ao Iniciar.");
     }else{
